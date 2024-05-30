d0665407708110e8f252482c9d8c1b289cf7e0fb
AraMaite <toloza.maite@hotmail.com>
2024-05-30 13:42:43 -0300

    correccion

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-let fecha = new Date("08/09/2024");
-<<<<<<< HEAD
-let msFecha = fecha.getTime();
+let fecha = new Date("2024/08/09");
+let msFecha = fecha.getTime();   
 
-let parrafoDias = document.querySelector ("#dias");
-let parrafoHoras = document.querySelector ("#horas");
-let parrafoMinutos = document.querySelector ("#minutos");
-let parrafoSegundos = document.querySelector ("#segundos");
-let parrafecha = document.querySelector ("#fecha");
-let cuentaAtras = document.querySelector ("#cuenta-atras");
+let parrafoDias = document.querySelector("#dias");
+let parrafoHoras = document.querySelector("#horas");
+let parrafoMinutos = document.querySelector("#minutos");
+let parrafoSegundos = document.querySelector("#segundos");
+let parrafecha = document.querySelector("#fecha");
+let cuentaAtras = document.querySelector("#cuenta-atras");
 
-spanFecha.inneText = fecha.toLocaleDateString();
+parrafecha.innerText = fecha.toLocaleDateString("es-ES");
 
 let Intervalo = setInterval(() => {
-
     let hoy = new Date().getTime();
-
     let distancia = msFecha - hoy;
     
     let msPorDia = 1000 * 60 * 60 * 24;
@@ -22,23 +19,19 @@ let Intervalo = setInterval(() => {
     let msPorMinuto = 1000 * 60;
     let msPorSegundo = 1000;
     
-    let dias = Math.floor (distancia / msPorDia);
-    let horas = Math.floor ((distancia % msPorDia) / msPorHora);
-    let minutos = Math.floor ((distancia % msPorHora) / msPorMinutos); 
-    let segundos =Math.floor ((distancia % msPorMinutos) / msPorSegundo);
+    let dias = Math.floor(distancia / msPorDia);
+    let horas = Math.floor((distancia % msPorDia) / msPorHora);
+    let minutos = Math.floor((distancia % msPorHora) / msPorMinuto); 
+    let segundos = Math.floor((distancia % msPorMinuto) / msPorSegundo);
 
-
-    parrafoDias.inneText = dias;
-    parrafoHoras.inneText = horas < 10 ? "0" + horas : horas; 
-    parrafoMinutos.inneText = minutos;
-    parrafoSegundos.inneText = segundos;
+    parrafoDias.innerText = dias;
+    parrafoHoras.innerText = horas < 10 ? "0" + horas : horas; 
+    parrafoMinutos.innerText = minutos;
+    parrafoSegundos.innerText = segundos;
 
     if (distancia < 0){
         clearInterval(Intervalo);
-        cuentaAtras.innerHTML = <p class="grande">¡LLEGO LE GRAN DIA!</p>
+        cuentaAtras.innerHTML = '<p class="grande">¡LLEGO EL GRAN DIA!</p>';
     }
     
-}, 1000)
-=======
-let msFecha = fecha.getTime();                
->>>>>>> 027f1d4e488167bf8c694eed8744affb6bb25ac6
+}, 1000);
